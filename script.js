64dc2acac2cb722566ec959de535504ca7a511a0
Basker12 <71788109+Basker12@users.noreply.github.com>
2021-04-15 18:40:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 function clickFunc() {
     document.getElementById("demo").innerHTML = "Hey you clicked the button well done!";
 }
+
+var x = document.getElementById("location");
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        x.innerHTML = "Your browser doesn't support this feature.";
+    }
+}
+
+function showPosition(position) {
+    x.innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;
+}
